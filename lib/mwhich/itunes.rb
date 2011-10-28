31ccfa05c4e34c08518fe8754395fc53f3e76934
Gun.io Whitespace Robot <contact@gun.io>
2011-10-28 15:45:35 -0400

    Remove whitespace [Gun.io WhitespaceBot]

--- lib/mwhich/itunes.rb
+++ lib/mwhich/itunes.rb
@@ -4,20 +4,20 @@ module MWhich
       def initialize(options={})
         @endpoint = "http://ax.phobos.apple.com.edgesuite.net/WebObjects/MZStoreServices.woa/wa/wsSearch"
       end
-      
+
       def search(title)
         results = request(title)
-        
+
         titles = []
         results.each do |result|
           titles << "#{result['kind']}: #{result['trackName']} ($#{result['trackPrice']})"
         end
-        
+
         titles
       end
-      
+
       protected
-        
+
         def request(title)
           # We'll do searches across both TV and movies and merge the results
           results = []
@@ -27,10 +27,10 @@ module MWhich
             data = Yajl::Parser.parse(response.body)
             results |= data['results']
           end
-          
+
           results
         end
-      
+
     end
   end
 end
